544a2ef971c76cf74df062cb4bef9597fdc4a2e1
syf814986678 <814986678@qq.com>
2020-03-14 21:43:34 +0800

    更新预测次数

--- prediction.py
+++ prediction.py
@@ -1,10 +1,10 @@
 import random
 while (1):
-    roundnum=0
+    roundnum=1
     end=0
-    guessnum=random.randint(0,10)
+    guessnum=random.randint(0,9)
     # print(guessnum)
-    while(end==0):
+    while(1):
         num=int(input("请输入您预测的数\n"))
         if(num<guessnum):
             print("遗憾，太小了\n")
@@ -14,7 +14,7 @@ while (1):
             print("遗憾，太大了\n")
             roundnum+=1
             continue
-        end=1
+        break
     print("预测"+str(roundnum)+"次，你猜中了！\n")
 
 
